f6eb6387a84eb8cea4b6e0ca8df22cf739ef4517
Mustafa Alkhazraji <mustafanawwar1199@gmail.com>
2026-01-03 16:17:24 +0300

    new projects

--- 01-AWS/cloud-init/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {
-  default = "us-west-1"
-}
-variable "instance_type" {}
-variable "production_vpc_cidr" {}
-variable "webapp_subnet_cidr" {}
-variable "webapp_subnet_AZ" {}
-variable "public_key_path" {}
